3dcc4de3dbc1dce0e895e25d59872977ada201d7
“tonykochev” <tonyakochev@lewisu.edu>
2025-09-18 10:13:23 -0500

    small changes

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 async function start(){
     const response = await fetch("https://dog.ceo/api/breeds/list/all")
     const data = await response.json
-    console.log(data)
+    creatBreedList(data.message)
 }
 
 start()
+
+function creatBreedList(breedList){
+
+}
