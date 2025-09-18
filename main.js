77478dc9d839569eee35a718f25651d2a0b5ab3a
“tonykochev” <tonyakochev@lewisu.edu>
2025-09-18 10:06:28 -0500

    change to the way api is called

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-fetch("https://dog.ceo/api/breeds/list/all").then(function(response){
-    return response.json()
-}).then(function(data){
+async function start(){
+    const response = await fetch("https://dog.ceo/api/breeds/list/all")
+    const data = await response.json
     console.log(data)
-})
-    
+}
+
+start()
